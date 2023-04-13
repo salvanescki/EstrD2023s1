7a6f918c649d6370f30f4f54ac9daa5977fd37c1
salvanescki <salvanescki.unq@gmail.com>
2023-04-13 20:45:10 -0300

    anda mal setv1

--- tp-5/SetV1.hs
+++ tp-5/SetV1.hs
@@ -2,22 +2,22 @@ module SetV1
     (Set, emptyS, addS, belongs, sizeS, removeS, unionS, setToList)
 where
 
-data Set a = S [a]
+data Set a = S [a] Int
 
-emptyS :: Set a
-addS :: Eq a => a -> Set a -> Set a
-belongs :: Eq a => a -> Set a -> Bool
-sizeS :: Eq a => Set a -> Int
-removeS :: Eq a => a -> Set a -> Set a
-unionS :: Eq a => Set a -> Set a -> Set a
-setToList :: Eq a => Set a -> [a]
+emptyS :: Set a                                             -- O(1)
+addS :: Eq a => a -> Set a -> Set a                         -- O(n)
+belongs :: Eq a => a -> Set a -> Bool                       -- O(n)
+sizeS :: Eq a => Set a -> Int                               -- O(1)
+removeS :: Eq a => a -> Set a -> Set a                      -- O(n)
+unionS :: Eq a => Set a -> Set a -> Set a                   -- O(n)
+setToList :: Eq a => Set a -> [a]                           -- O(1)
 
 pertenece :: Eq a => a -> [a] -> Bool
 pertenece _ [] = False
 pertenece e (x:xs) = e == x || pertenece e xs
 
 agregarSiNoEsta :: Eq a => a -> Set a -> Set a
-agregarSiNoEsta x (S xs) = if pertenece x xs then (S xs) else (S (x:xs))
+agregarSiNoEsta x (S xs l) = if pertenece x xs then (S xs l) else (S (x:xs) (l+1))
 
 sacar :: Eq a => a -> [a] -> [a]
 sacar n [] = []
@@ -29,10 +29,10 @@ unionL :: Eq a => [a] -> [a] -> [a]
 unionL [] ys = ys
 unionL (x:xs) ys = if pertenece x ys then unionL xs ys else x : unionL xs ys
 
-emptyS = S []
+emptyS = S [] 0
 addS x s = agregarSiNoEsta x s
-belongs x (S xs) = pertenece x xs
-sizeS (S xs) = length xs
-removeS x (S xs) = S (sacar x xs)
-unionS (S xs) (S ys) = S (unionL xs ys)
-setToList (S xs) = xs
+belongs x (S xs l) = pertenece x xs
+sizeS (S xs l) = l
+removeS x (S xs l) = S (sacar x xs) (l - 1)
+unionS (S xs l1) (S ys l2) = S (unionL xs ys) (length (unionL xs ys)) -- #xs + #ys - #(xs n ys)
+setToList (S xs l) = xs
