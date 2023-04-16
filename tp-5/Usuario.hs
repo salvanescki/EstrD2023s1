0e58a99aaff226de59ed7ac638e57424c253b89a
salvanescki <salvanescki.unq@gmail.com>
2023-04-16 18:26:05 -0300

    queue listo

--- tp-5/Usuario.hs
+++ tp-5/Usuario.hs
@@ -1,13 +1,17 @@
 import SetV1 -- Set, emptyS, addS, belongs, sizeS, removeS, unionS, setToList
+import QueueV1 -- Queue, emptyQ, isEmptyQ, enqueue, firstQ, dequeue
 
 -- Library
 
 agregarSi :: Eq a => a -> [a] -> Bool -> [a]
 agregarSi x xs c = if c then (x:xs) else xs
 
--- Ejemplo Set
+-- Ejemplos
 
 set1 = addS 3 (addS 2 (addS 1 emptyS))
+
+queue1 = enqueue 4 (enqueue 3 (enqueue 2 (enqueue 1 emptyQ)))
+queue2 = enqueue 8 (enqueue 7 (enqueue 6 (enqueue 5 emptyQ)))
 
 --
 
@@ -35,3 +39,16 @@ unirTodos EmptyT = emptyS
 unirTodos (NodeT s t1 t2) = unionS s (unionS (unirTodos t1) (unirTodos t2))
 
 --
+
+lengthQ :: Queue a -> Int
+lengthQ q = if (isEmptyQ q) then 0 else 1 + lengthQ (dequeue q)
+
+--
+
+queueToList :: Queue a -> [a]
+queueToList q = if (isEmptyQ q) then [] else firstQ q : queueToList (dequeue q)
+
+--
+
+unionQ :: Queue a -> Queue a -> Queue a
+unionQ q1 q2 = if (isEmptyQ q2) then q1 else enqueue (firstQ q2) (unionQ q1 (dequeue q2))
